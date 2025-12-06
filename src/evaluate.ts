aa440106608de853f2d40b50ac76453c572eb6ff
Marco Nikander <m4n1k.developer@gmail.com>
2025-12-06 18:25:35 +0100

    enum to access instruction properties

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -16,52 +16,52 @@ export function evaluate(instructions: readonly Instruction[]): RawValue {
         const instruc: Instruction       = instructions[pc];
         const reg: (undefined | Value)[] = top(stack).registers;
 
-        switch (instruc[1]) {
+        switch (instruc[Get.Tag]) {
             case 'Const':
-                reg[instruc[0]] = { tag: 'Value', value: instruc[2] };
+                reg[instruc[Get.Dest]] = { tag: 'Value', value: instruc[Get.Left] };
                 break;
             case 'Copy':
-                reg[instruc[0]] = reg[instruc[2]];
+                reg[instruc[Get.Dest]] = reg[instruc[Get.Left]];
                 break;
             case 'Add':
-                reg[instruc[0]] = { tag: 'Value', value: assert_number(reg[instruc[2]]) + assert_number(reg[instruc[3]]) };
+                reg[instruc[Get.Dest]] = { tag: 'Value', value: assert_number(reg[instruc[Get.Left]]) + assert_number(reg[instruc[Get.Right]]) };
                 break;
             case 'Label':
                 break;
             case 'Jump':
-                pc = find_label(instructions, instruc[2]);
+                pc = find_label(instructions, instruc[Get.Left]);
                 break;
             case 'Branch':
-                if (assert_boolean(reg[instruc[2]])) {
-                    pc = find_label(instructions, instruc[3]);
+                if (assert_boolean(reg[instruc[Get.Left]])) {
+                    pc = find_label(instructions, instruc[Get.Right]);
                 }
                 break;
             case 'Function':
-                throw Error(`Encountered unexpected function body of '${instruc[2]}'.`)
+                throw Error(`Encountered unexpected function body of '${instruc[Get.Left]}'.`)
             case 'Call':
                 // TODO: add arity check when calling a function
                 stack.push(
-                    { registers: instruc[3].map((r) => {return reg[r];}),
+                    { registers: instruc[Get.Right].map((r) => {return reg[r];}),
                       return_pc: pc }
                     );
-                pc = find_label(instructions, instruc[2]);
+                pc = find_label(instructions, instruc[Get.Left]);
                 break;
             case 'Return':
                 pc = assert_defined(top(stack).return_pc);
-                peek(stack).registers[(instructions[pc] as Call)[0]] = reg[instruc[2]];;
+                peek(stack).registers[(instructions[pc] as Call)[Get.Dest]] = reg[instruc[Get.Left]];;
                 stack.pop();
                 break;
             case 'Exit':
-                return assert_defined(reg[instruc[2]]).value;
+                return assert_defined(reg[instruc[Get.Left]]).value;
             default:
-                throw Error(`Unhandled instruction type '${(instruc as Instruction)[1]}'`);
+                throw Error(`Unhandled instruction type '${(instruc as Instruction)[Get.Tag]}'`);
         }
     }
     throw Error(`Reached end of instructions without an 'Exit' command`);
 }
 
 function find_label(instructions: readonly Instruction[], label: string): number {
-    return instructions.findIndex((i: Instruction) => { return (i[1] === 'Label' || i[1] === 'Function') && i[2] === label; });
+    return instructions.findIndex((i: Instruction) => { return (i[Get.Tag] === 'Label' || i[Get.Tag] === 'Function') && i[Get.Left] === label; });
 }
 
 function top(stack: Frame[]): Frame {
@@ -70,4 +70,11 @@ function top(stack: Frame[]): Frame {
 
 function peek(stack: Frame[]): Frame {
     return assert_defined(stack[stack.length - 2]);
+}
+
+enum Get {
+    Dest  = 0,
+    Tag   = 1,
+    Left  = 2,
+    Right = 3,
 }
