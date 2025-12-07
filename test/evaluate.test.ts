5d27c45bed66e174aa9e08de01fdd0ba84517c59
Marco Nikander <m4n1k.developer@gmail.com>
2025-12-07 10:13:50 +0100

    use larger constants in the unit tests, to better distinguish them from register numbers

--- test/evaluate.test.ts
+++ test/evaluate.test.ts
@@ -11,75 +11,75 @@ describe('constants and exit', () => {
 
     it('must throw error if there is no Exit instruction', () => {
         const input: Instruction[] = [
-            [    0, 'Const', 42 ],
+            [    0, 'Const', 11 ],
         ];
         expect(() => evaluate(input)).toThrow();
     });
 
     it('must evaluate a constant', () => {
         const input: Instruction[] = [
-            [    0, 'Const', 42 ],
+            [    0, 'Const', 11 ],
             [ null, 'Exit',  0 ],
         ];
-        expect(evaluate(input)).toBe(42);
+        expect(evaluate(input)).toBe(11);
     });
 });
 
 describe('copying of registers', () => {
     it('must copy a constant', () => {
         const input: Instruction[] = [
-            [    0, 'Const', 42 ],
+            [    0, 'Const', 11 ],
             [    1, 'Copy',  0 ],
             [ null, 'Exit',  1 ],
         ];
-        expect(evaluate(input)).toBe(42);
+        expect(evaluate(input)).toBe(11);
     });
 });
 
 describe('arithmetic operations', () => {
     it('must evaluate integer addition', () => {
         const input: Instruction[] = [
-            [    0, 'Const', 1 ],
-            [    1, 'Const', 2 ],
+            [    0, 'Const', 11 ],
+            [    1, 'Const', 22 ],
             [    2, 'Add',   0, 1 ],
             [ null, 'Exit',  2 ],
         ];
-        expect(evaluate(input)).toBe(3);
+        expect(evaluate(input)).toBe(33);
     });
 });
 
 describe('labels, jump, and branch', () => {
     it('must skip over labels as if they are a "no-op"', () => {
         const input: Instruction[] = [
-            [    0, 'Const', 1 ],
+            [    0, 'Const', 11 ],
             [ null, 'Label', 'First'],
-            [    1, 'Const', 2 ],
+            [    1, 'Const', 22 ],
             [ null, 'Label', 'Second'],
             [    2, 'Add',   0, 1 ],
             [ null, 'Label', 'Third'],
             [ null, 'Exit',  2 ],
         ];
-        expect(evaluate(input)).toBe(3);
+        expect(evaluate(input)).toBe(33);
     });
 
     it('must execute the correct line of code after an unconditional jump ', () => {
         const input: Instruction[] = [
             [ null, 'Jump',  'Second'],
             [ null, 'Label', 'First'],
-            [    1, 'Const', 1 ],
+            [    1, 'Const', 11 ],
             [ null, 'Label', 'Second'],
-            [    2, 'Const', 2 ],
+            [    2, 'Const', 22 ],
             [ null, 'Exit',  2 ],
         ];
-        expect(evaluate(input)).toBe(2);
+        expect(evaluate(input)).toBe(22);
     });
 
     it('must not branch when condition is false', () => {
         const input: Instruction[] = [
             [    0, 'Const',  false ],
-            [    1, 'Const',  1 ],
-            [    2, 'Const',  2 ],
-            [    3, 'Const',  4 ],
+            [    1, 'Const',  11 ],
+            [    2, 'Const',  22 ],
+            [    3, 'Const',  44 ],
             [ null, 'Branch', 'Else', 0 ],
             [    4, 'Add',    1, 2 ],
             [ null, 'Jump',  'End' ],
@@ -88,15 +88,15 @@ describe('labels, jump, and branch', () => {
             [ null, 'Label', 'End'],
             [ null, 'Exit',   4 ],
         ];
-        expect(evaluate(input)).toBe(3);
+        expect(evaluate(input)).toBe(33);
     });
 
     it('must branch when condition is true', () => {
         const input: Instruction[] = [
             [    0, 'Const',  true ],
-            [    1, 'Const',  1 ],
-            [    2, 'Const',  2 ],
-            [    3, 'Const',  4 ],
+            [    1, 'Const',  11 ],
+            [    2, 'Const',  22 ],
+            [    3, 'Const',  44 ],
             [ null, 'Branch', 'Else' , 0],
             [    4, 'Add',    1, 2 ],
             [ null, 'Jump',   'End' ],
@@ -105,41 +105,41 @@ describe('labels, jump, and branch', () => {
             [ null, 'Label',  'End'],
             [ null, 'Exit',   5 ],
         ];
-        expect(evaluate(input)).toBe(6);
+        expect(evaluate(input)).toBe(66);
     });
 });
 
 describe('function call', () => {
     it('must support calling the identity function', () => {
         const input: Instruction[] = [
-            [    0, 'Const', 0 ],
-            [    1, 'Const', 42 ],
+            [    0, 'Const', 11 ],
+            [    1, 'Const', 22 ],
             [    2, 'Call',  'identity', [1] ],
             [ null, 'Exit',  2 ],
             [ null, 'Function', 'identity', ['a'] ],
             [ null, 'Return', 0 ], // return register (i.e. argument) 0
         ];
-        expect(evaluate(input)).toBe(42);
+        expect(evaluate(input)).toBe(22);
     });
 
     it('must support calling a binary function', () => {
         const input: Instruction[] = [
-            [    0, 'Const', 10 ],
-            [    1, 'Const', 20 ],
+            [    0, 'Const', 11 ],
+            [    1, 'Const', 22 ],
             [    2, 'Call',  'first', [0, 1] ],
             [ null, 'Exit',  2 ],
             [ null, 'Function', 'first', ['a', 'b'] ],
             [ null, 'Return', 0 ], // return register (i.e. argument) 0
         ];
-        expect(evaluate(input)).toBe(10);
+        expect(evaluate(input)).toBe(11);
     });
 });
 
 describe('static single assignment', () => {
     it('must throw an error when re-assigning to a register', () => {
         const input: Instruction[] = [
-            [    0, 'Const', 10 ],
-            [    0, 'Const', 20 ], // attempt to reassign register 0
+            [    0, 'Const', 11 ],
+            [    0, 'Const', 22 ], // attempt to reassign register 0
             [ null, 'Exit',  1 ],
         ];
         expect(() => {evaluate(input)}).toThrow();
