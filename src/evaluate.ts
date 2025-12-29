28bfba8149570e66cf63a06fa229595d3fd854c9
Marco Nikander <m4n1k.developer@gmail.com>
2025-12-29 16:51:54 +0100

    rename 'instructions' to 'program'

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -7,17 +7,17 @@ import { arithmetic, comparison } from "./basic_operations.ts";
 
 type Frame = { registers: Map<Register, Value>, return_pc: undefined | number, return_block: undefined | string };
 
-export function evaluate(instructions: readonly Instruction[]): RawValue {
-    instructions = verify_single_assignment(instructions);
+export function evaluate(program: readonly Instruction[]): RawValue {
+    program = verify_single_assignment(program);
     const stack: Frame[] = [ {registers: new Map<Register, Value>(), return_pc: undefined, return_block: undefined } ];
     let pc: number                         = 0;
     let current_block: string              = 'Entry';
     let previous_block: undefined | string = undefined;
 
     try {
-        while (pc < instructions.length) {
+        while (pc < program.length) {
             if (top(stack) === undefined) throw Error(`Bug -- no valid stack frame`);
-            const instruc: Instruction       = instructions[pc];
+            const instruc: Instruction       = program[pc];
             const reg: Map<Register, Value>  = top(stack).registers;
             const dest: null | Register      = instruc[Get.Dest];
     
@@ -50,56 +50,56 @@ export function evaluate(instructions: readonly Instruction[]): RawValue {
                     reg.set(valid(dest), comparison(instruc, reg, (l, r) => { return l !== r; }));
                     break;
                 case 'Jump': {
-                    pc = find_label(instructions, instruc[Get.Left]);
+                    pc = find_label(program, instruc[Get.Left]);
                     previous_block = current_block;
-                    current_block  = (instructions[pc] as Label)[Get.Left];
+                    current_block  = (program[pc] as Label)[Get.Left];
                     break;
                 }
                 case 'Branch': {
                     const condition = get_boolean(reg.get(instruc[Get.Last]));
                     if (condition) {
-                        pc = find_label(instructions, instruc[Get.Left]);
+                        pc = find_label(program, instruc[Get.Left]);
                     }
                     else {
-                        pc = find_label(instructions, instruc[Get.Right]);
+                        pc = find_label(program, instruc[Get.Right]);
                     }
                     previous_block = current_block;
-                    current_block  = (instructions[pc] as Label)[Get.Left];
+                    current_block  = (program[pc] as Label)[Get.Left];
                     break;
                 }
                 case 'Call': {
                     stack.push({ registers: new Map<Register, Value>(),
                                  return_pc: pc,
                                  return_block: current_block });
-                    pc = find_label(instructions, instruc[Get.Left]);
-                    if (instruc[Get.Right]?.length !== instructions[pc][Get.Right]?.length) {
-                        throw Error(`Line ${top(stack).return_pc}: function '${instructions[pc][Get.Left]}' expects ${instructions[pc][Get.Right]?.length} arguments, got ${instruc[Get.Right]?.length}`);
+                    pc = find_label(program, instruc[Get.Left]);
+                    if (instruc[Get.Right]?.length !== program[pc][Get.Right]?.length) {
+                        throw Error(`Line ${top(stack).return_pc}: function '${program[pc][Get.Left]}' expects ${program[pc][Get.Right]?.length} arguments, got ${instruc[Get.Right]?.length}`);
                     }
                     // copy register contents into new frame as function arguments, with the parameter names as their register names
                     for (let i: number = 0; i < instruc[Get.Right]?.length; i++) {
-                        const parameter: Register = (instructions[pc] as Function)[Get.Right][i];
+                        const parameter: Register = (program[pc] as Function)[Get.Right][i];
                         const value: Value        = valid(reg.get(instruc[Get.Right][i]));
                         top(stack).registers.set(parameter, value);
                     }
                     previous_block = current_block;
-                    current_block  = (instructions[pc] as Function)[Get.Left];
+                    current_block  = (program[pc] as Function)[Get.Left];
                     break;
                 }
                 case 'Return': {
                     pc                  = valid(top(stack).return_pc);
                     previous_block      = current_block;
                     current_block       = valid(top(stack).return_block);
-                    const call: Call    = instructions[pc] as Call;
+                    const call: Call    = program[pc] as Call;
                     const result: Value = valid(reg.get(instruc[Get.Left]));
                     previous(stack).registers.set(call[Get.Dest], result);
                     stack.pop();
                     break;
                 }
                 case 'Phi': {
-                    if (previous_block === find_label_for_register(instructions, instruc[Get.Left])) {
+                    if (previous_block === find_label_for_register(program, instruc[Get.Left])) {
                         reg.set(valid(dest), valid(reg.get(instruc[Get.Left])));
                     }
-                    else if (previous_block === find_label_for_register(instructions, instruc[Get.Right])) {
+                    else if (previous_block === find_label_for_register(program, instruc[Get.Right])) {
                         reg.set(valid(dest), valid(reg.get(instruc[Get.Right])));
                     }
                     else {
@@ -118,7 +118,7 @@ export function evaluate(instructions: readonly Instruction[]): RawValue {
             }
             pc++;
         }
-        throw Error(`reached end of instructions without an 'Exit' command`);
+        throw Error(`reached end of program without an 'Exit' command`);
     }
     catch (error) {
         // catch and then re-throw all errors, with the line-number prepended, for easier debugging
@@ -134,15 +134,15 @@ function previous(stack: Frame[]): Frame {
     return valid(stack[stack.length - 2]);
 }
 
-function find_label(instructions: readonly Instruction[], label: string): number {
+function find_label(program: readonly Instruction[], label: string): number {
     const isCorrectLabelOrFunction = (inst: Instruction) => { return (inst[Get.Tag] === 'Label' || inst[Get.Tag] === 'Function') && inst[Get.Left] === label; };
-    return instructions.findIndex(isCorrectLabelOrFunction);
+    return program.findIndex(isCorrectLabelOrFunction);
 }
 
-function find_label_for_register(instructions: readonly Instruction[], register: Register): string {
+function find_label_for_register(program: readonly Instruction[], register: Register): string {
     let label: string = 'Entry';
-    for (let line: number = 0; line < instructions.length; line++) {
-        const instruc: Instruction  = instructions[line];
+    for (let line: number = 0; line < program.length; line++) {
+        const instruc: Instruction  = program[line];
         const dest: null | Register = instruc[Get.Dest];
 
         if (instruc[Get.Tag] === 'Label' || instruc[Get.Tag] === 'Function') {
