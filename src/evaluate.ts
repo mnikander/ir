13b29da0e7aa927bd84338673d1b37e11a22327b
Marco Nikander <m4n1k.developer@gmail.com>
2025-12-30 09:40:11 +0100

    interpreter state as an object

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -11,18 +11,27 @@ type Frame = {
     return_block: undefined | string,
 };
 
+type State = {
+    stack: Frame[],
+    pc: number,
+    current_block: string,
+    previous_block: undefined | string,
+};
+
 export function evaluate(program: readonly Instruction[]): RawValue {
     program = verify_single_assignment(program);
-    const stack: Frame[] = [ {registers: new Map<Register, Value>(), return_pc: undefined, return_block: undefined } ];
-    let pc: number                         = 0;
-    let current_block: string              = '@Entry';
-    let previous_block: undefined | string = undefined;
+    const state: State = {
+        stack: [ {registers: new Map<Register, Value>(), return_pc: undefined, return_block: undefined } ],
+        pc: 0,
+        current_block: '@Entry',
+        previous_block: undefined,
+    };
 
     try {
-        while (pc < program.length) {
-            if (top(stack) === undefined) throw Error(`Bug -- no valid stack frame`);
-            const line: Instruction          = program[pc];
-            const reg: Map<Register, Value>  = top(stack).registers;
+        while (state.pc < program.length) {
+            if (top(state.stack) === undefined) throw Error(`Bug -- no valid stack frame`);
+            const line: Instruction          = program[state.pc];
+            const reg: Map<Register, Value>  = top(state.stack).registers;
             const dest: null | Register      = line[Get.Dest];
     
             switch (line[Get.Tag]) {
@@ -36,65 +45,65 @@ export function evaluate(program: readonly Instruction[]): RawValue {
                 case 'Equal':     reg.set(valid(dest), equal(line, reg));     break;
                 case 'Unequal':   reg.set(valid(dest), unequal(line, reg));   break;
                 case 'Jump': {
-                    pc = find_label(program, line[Get.Left]);
-                    previous_block = current_block;
-                    current_block  = (program[pc] as Label)[Get.Left];
+                    state.pc             = find_label(program, line[Get.Left]);
+                    state.previous_block = state.current_block;
+                    state.current_block  = (program[state.pc] as Label)[Get.Left];
                     break;
                 }
                 case 'Branch': {
                     const condition = get_boolean(reg.get(line[Get.Last]));
                     if (condition) {
-                        pc = find_label(program, line[Get.Left]);
+                        state.pc = find_label(program, line[Get.Left]);
                     }
                     else {
-                        pc = find_label(program, line[Get.Right]);
+                        state.pc = find_label(program, line[Get.Right]);
                     }
-                    previous_block = current_block;
-                    current_block  = (program[pc] as Label)[Get.Left];
+                    state.previous_block = state.current_block;
+                    state.current_block  = (program[state.pc] as Label)[Get.Left];
                     break;
                 }
                 case 'Call': {
-                    const new_pc: number = find_label(program, line[Get.Left]);
+                    const new_pc: number   = find_label(program, line[Get.Left]);
                     const provided: number = valid(line[Get.Right]).length;
                     const expected: number = valid(program[new_pc][Get.Right]).length;
                     if (provided !== expected) {
                         throw Error(`function '${program[new_pc][Get.Left]}' expects ${expected} arguments, got ${provided}`);
                     }
-                    stack.push({ registers: new Map<Register, Value>(),
-                                 return_pc: pc,
-                                 return_block: current_block });
-                    pc = new_pc;
+                    state.stack.push({ registers: new Map<Register, Value>(),
+                                 return_pc: state.pc,
+                                 return_block: state.current_block });
+                    state.pc = new_pc;
                     // copy register contents into new frame, as function arguments
                     for (let i: number = 0; i < line[Get.Right]?.length; i++) {
-                        const parameter: Register = (program[pc] as Function)[Get.Right][i];
+                        const parameter: Register = (program[state.pc] as Function)[Get.Right][i];
                         const value: Value        = valid(reg.get(line[Get.Right][i]));
-                        top(stack).registers.set(parameter, value);
+                        top(state.stack).registers.set(parameter, value);
                     }
-                    previous_block = current_block;
-                    current_block  = (program[pc] as Function)[Get.Left];
+                    state.previous_block = state.current_block;
+                    state.current_block  = (program[state.pc] as Function)[Get.Left];
                     break;
                 }
                 case 'Return': {
-                    pc                  = valid(top(stack).return_pc);
-                    previous_block      = current_block;
-                    current_block       = valid(top(stack).return_block);
-                    const call: Call    = program[pc] as Call;
-                    const result: Value = valid(reg.get(line[Get.Left]));
-                    previous(stack).registers.set(call[Get.Dest], result);
-                    stack.pop();
+                    state.pc             = valid(top(state.stack).return_pc);
+                    state.previous_block = state.current_block;
+                    state.current_block  = valid(top(state.stack).return_block);
+                    const call: Call     = program[state.pc] as Call;
+                    const result: Value  = valid(reg.get(line[Get.Left]));
+                    previous(state.stack).registers.set(call[Get.Dest], result);
+                    state.stack.pop();
                     break;
                 }
                 case 'Phi': {
                     const left:  Register = line[Get.Left];
                     const right: Register = line[Get.Right];
-                    if (previous_block === find_label_for_register(program, left)) {
+                    if (state.previous_block === find_label_for_register(program, left)) {
                         reg.set(valid(dest), valid(reg.get(left)));
                     }
-                    else if (previous_block === find_label_for_register(program, right)) {
+                    else if (state.previous_block === find_label_for_register(program, right)) {
                         reg.set(valid(dest), valid(reg.get(right)));
                     }
                     else {
-                        throw Error(`cannot compute Phi(${left}, ${right}) when previous block is '${previous_block}'.`)
+                        throw Error(`cannot compute Phi(${left}, ${right}) when previous block is '${state.previous_block}'.`)
                     }
                     break;
                 }
@@ -107,13 +116,13 @@ export function evaluate(program: readonly Instruction[]): RawValue {
                 default:
                     throw Error(`unhandled instruction type '${(line as Instruction)[Get.Tag]}'`);
             }
-            pc++;
+            state.pc++;
         }
         throw Error(`reached end of program without an 'Exit' command`);
     }
     catch (error) {
         // catch and then re-throw all errors, with the line-number prepended, for easier debugging
-        throw Error(`Line ${pc}: ` + (error as Error).message);
+        throw Error(`Line ${state.pc}: ` + (error as Error).message);
     }
 }
 
