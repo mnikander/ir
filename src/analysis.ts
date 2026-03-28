da31b533e98817e958064a16714b823de8f17591
Marco Nikander <m4n1k.developer@gmail.com>
2026-03-28 11:28:18 +0100

    rename a function

--- src/analysis.ts
+++ src/analysis.ts
@@ -26,17 +26,17 @@ function verify_single_assignment(instructions: readonly Instruction[]): readonl
     for (let line: number = 0; line < instructions.length; ++line) {
         const destination: null | Register = instructions[line][Get.Dest];
         if(destination !== null) {
-            assign(destination, assigned_registers, line);
+            assign_register(destination, assigned_registers, line);
         }
         else if (instructions[line][Get.Tag] === 'Function') {
             const args: Register[] = (instructions[line] as Function)[Get.Right];
-            args.forEach((arg: Register) => { assign(arg, assigned_registers, line)});
+            args.forEach((arg: Register) => { assign_register(arg, assigned_registers, line)});
         }
     }
     return instructions;
 }
 
-function assign(register: Register, assigned_registers: Set<Register>, line: number): Set<Register> {
+function assign_register(register: Register, assigned_registers: Set<Register>, line: number): Set<Register> {
     if (assigned_registers.has(register)) {
         throw Error(`Line ${line}: Register ${register} is already assigned`);
     }
