eaa6cfac9754c220e3cffb8649ad014d4fef8e5a
Marco Nikander <m4n1k.developer@gmail.com>
2026-04-05 16:43:28 +0200

    proto: rename Value to Register

--- proto/grammar.ts
+++ proto/grammar.ts
@@ -1,48 +1,48 @@
 // Copyright (c) 2026 Marco Nikander
 
 export type Program      = Function[];
-export type Function     = { func: Label,        params: Input[],      blocks: Block[] };
-export type Block        = { block: Label,       joins: Phi[],         lines: Line[], terminator: Terminator };
+export type Function     = { func: Label,           params: Input[],      blocks: Block[] };
+export type Block        = { block: Label,          joins: Phi[],         lines: Line[], terminator: Terminator };
 export type Line         = Call | Ownership | Arithmetic | Comparison;
 
-export type Phi          = [ destination: Value, tag: 'Phi',           inputs: [label: Label, value: Input][]];
-export type Call         = [ destination: Value, tag: 'Call',          label: Label, arguments: Input[] ];
+export type Phi          = [ destination: Register, tag: 'Phi',           inputs: [label: Label, value: Input][]];
+export type Call         = [ destination: Register, tag: 'Call',          label: Label, arguments: Input[] ];
 
 export type Ownership    = Define | Stack | Heap | Borrow | Dereference | Update | Drop;
-export type Define       = [ destination: Value, tag: 'Define',        value: Input ];
-export type Stack        = [ destination: Value, tag: 'Stack',         value: Input ];
-export type Heap         = [ destination: Value, tag: 'Heap',          value: Input ];
-export type Borrow       = [ destination: Value, tag: 'Borrow',        pointer: Value ];
-export type Dereference  = [ destination: Value, tag: 'Dereference',   pointer: Value ];
-export type Update       = [ destination: Value, tag: 'Update',        pointer: Value, value: Value ];
-export type Drop         = [ destination: null,  tag: 'Drop',          value: Value ];
+export type Define       = [ destination: Register, tag: 'Define',        value: Input ];
+export type Stack        = [ destination: Register, tag: 'Stack',         value: Input ];
+export type Heap         = [ destination: Register, tag: 'Heap',          value: Input ];
+export type Borrow       = [ destination: Register, tag: 'Borrow',        pointer: Register ];
+export type Dereference  = [ destination: Register, tag: 'Dereference',   pointer: Register ];
+export type Update       = [ destination: Register, tag: 'Update',        pointer: Register, value: Register ];
+export type Drop         = [ destination: null,     tag: 'Drop',          value: Register ];
 
 export type Arithmetic   = Add | Subtract | Multiply | Divide | Remainder | Minimum | Maximum | Negative;
-export type Add          = [ destination: Value, tag: 'Add',           left: Input, right: Input ];
-export type Subtract     = [ destination: Value, tag: 'Subtract',      left: Input, right: Input ];
-export type Multiply     = [ destination: Value, tag: 'Multiply',      left: Input, right: Input ];
-export type Divide       = [ destination: Value, tag: 'Divide',        left: Input, right: Input ];
-export type Remainder    = [ destination: Value, tag: 'Remainder',     left: Input, right: Input ];
-export type Minimum      = [ destination: Value, tag: 'Minimum',       left: Input, right: Input ];
-export type Maximum      = [ destination: Value, tag: 'Maximum',       left: Input, right: Input ];
-export type Negative     = [ destination: Value, tag: 'Negate',        left: Input ];
+export type Add          = [ destination: Register, tag: 'Add',           left: Input, right: Input ];
+export type Subtract     = [ destination: Register, tag: 'Subtract',      left: Input, right: Input ];
+export type Multiply     = [ destination: Register, tag: 'Multiply',      left: Input, right: Input ];
+export type Divide       = [ destination: Register, tag: 'Divide',        left: Input, right: Input ];
+export type Remainder    = [ destination: Register, tag: 'Remainder',     left: Input, right: Input ];
+export type Minimum      = [ destination: Register, tag: 'Minimum',       left: Input, right: Input ];
+export type Maximum      = [ destination: Register, tag: 'Maximum',       left: Input, right: Input ];
+export type Negative     = [ destination: Register, tag: 'Negate',        left: Input ];
 
 export type Comparison   = Equal | Unequal | Less | LessEqual | Greater | GreaterEqual;
-export type Equal        = [ destination: Value, tag: 'Equal',         left: Input, right: Input ];
-export type Unequal      = [ destination: Value, tag: 'Unequal',       left: Input, right: Input ];
-export type Less         = [ destination: Value, tag: 'Less',          left: Input, right: Input ];
-export type LessEqual    = [ destination: Value, tag: 'LessEqual',     left: Input, right: Input ];
-export type Greater      = [ destination: Value, tag: 'Greater',       left: Input, right: Input ];
-export type GreaterEqual = [ destination: Value, tag: 'GreaterEqual',  left: Input, right: Input ];
+export type Equal        = [ destination: Register, tag: 'Equal',         left: Input, right: Input ];
+export type Unequal      = [ destination: Register, tag: 'Unequal',       left: Input, right: Input ];
+export type Less         = [ destination: Register, tag: 'Less',          left: Input, right: Input ];
+export type LessEqual    = [ destination: Register, tag: 'LessEqual',     left: Input, right: Input ];
+export type Greater      = [ destination: Register, tag: 'Greater',       left: Input, right: Input ];
+export type GreaterEqual = [ destination: Register, tag: 'GreaterEqual',  left: Input, right: Input ];
 
 export type Terminator   = Jump | Branch | Return | Exit;
-export type Jump         = [ destination: null,  tag: 'Jump',          block: Label ];
-export type Branch       = [ destination: null,  tag: 'Branch',        condition: Input, block: [Label, Label] ];
-export type Return       = [ destination: null,  tag: 'Return',        left: Input ];
-export type Exit         = [ destination: null,  tag: 'Exit',          left: Input ];
+export type Jump         = [ destination: null,     tag: 'Jump',          block: Label ];
+export type Branch       = [ destination: null,     tag: 'Branch',        condition: Input, block: [Label, Label] ];
+export type Return       = [ destination: null,     tag: 'Return',        left: Input ];
+export type Exit         = [ destination: null,     tag: 'Exit',          left: Input ];
 
-export type Input        = [number] | [Value] | ['Move', Value];
-export type Value        = `%${string}`;
+export type Input        = [number] | [Register] | ['Move', Register];
+export type Register     = `%${string}`;
 export type Label        = `@${string}`;
 
 export enum Get {
