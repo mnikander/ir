fee9f52e174516c4ec53c44b6a03a7eda7992c8f
Marco Nikander <m4n1k.developer@gmail.com>
2026-04-10 22:10:19 +0200

    ref: rename 'line' to 'op' to avoid confusion with line numbers

--- src/runtime/machine.ts
+++ src/runtime/machine.ts
@@ -41,32 +41,32 @@ export function evaluate(program: LIR.Program): LIR.Primitive {
 
   try {
     while (stack.frames.length > 1) {
-      const line: LIR.Instruction = program[top(stack).pc];
-      switch (line[LIR.Get.Tag]) {
-        case "Constant":     stack =      constant(stack, line); break;
-        case "Copy":         stack =          copy(stack, line); break;
-        case "Load":         stack =          load(stack, line); break;
-        case "Store":        stack =         store(stack, line); break;
-        case "Alloc":        stack =         alloc(stack, line); break;
-        case "Add":          stack =           add(stack, line); break;
-        // case 'Subtract':     stack =      subtract(stack, line); break;
-        // case 'Multiply':     stack =      multiply(stack, line); break;
-        // case 'Divide':       stack =        divide(stack, line); break;
-        // case 'Remainder':    stack =     remainder(stack, line); break;
-        // case 'Minimum':      stack =       minimum(stack, line); break;
-        // case 'Maximum':      stack =       maximum(stack, line); break;
-        // case 'Negate':       stack =      negative(stack, line); break;
-        // case 'Equal':        stack =         equal(stack, line); break;
-        // case 'Unequal':      stack =       unequal(stack, line); break;
-        // case 'Less':         stack =          less(stack, line); break;
-        // case 'LessEqual':    stack =    less_equal(stack, line); break;
-        // case 'Greater':      stack =       greater(stack, line); break;
-        // case 'GreaterEqual': stack = greater_equal(stack, line); break;
-        case "Jump":         stack =          jump(stack, line); break;
-        case 'Branch':       stack =        branch(stack, line); break;
-        // case 'Call':         stack =          call(stack, line, program); break; // TODO: implement next
-        case "Return":       stack = ret(stack, line); break;
-        default: throw Error(`unhandled instruction type '${(line as LIR.Instruction)[LIR.Get.Tag]}'`);
+      const op: LIR.Instruction = program[top(stack).pc];
+      switch (op[LIR.Get.Tag]) {
+        case "Constant":     stack =      constant(stack, op); break;
+        case "Copy":         stack =          copy(stack, op); break;
+        case "Load":         stack =          load(stack, op); break;
+        case "Store":        stack =         store(stack, op); break;
+        case "Alloc":        stack =         alloc(stack, op); break;
+        case "Add":          stack =           add(stack, op); break;
+        // case 'Subtract':     stack =      subtract(stack, op); break;
+        // case 'Multiply':     stack =      multiply(stack, op); break;
+        // case 'Divide':       stack =        divide(stack, op); break;
+        // case 'Remainder':    stack =     remainder(stack, op); break;
+        // case 'Minimum':      stack =       minimum(stack, op); break;
+        // case 'Maximum':      stack =       maximum(stack, op); break;
+        // case 'Negate':       stack =      negative(stack, op); break;
+        // case 'Equal':        stack =         equal(stack, op); break;
+        // case 'Unequal':      stack =       unequal(stack, op); break;
+        // case 'Less':         stack =          less(stack, op); break;
+        // case 'LessEqual':    stack =    less_equal(stack, op); break;
+        // case 'Greater':      stack =       greater(stack, op); break;
+        // case 'GreaterEqual': stack = greater_equal(stack, op); break;
+        case "Jump":         stack =          jump(stack, op); break;
+        case 'Branch':       stack =        branch(stack, op); break;
+        // case 'Call':         stack =          call(stack, op, program); break; // TODO: implement next
+        case "Return":       stack = ret(stack, op); break;
+        default: throw Error(`unhandled instruction type '${(op as LIR.Instruction)[LIR.Get.Tag]}'`);
       }
     }
     return to_value(stack.data[0]).value;
@@ -76,46 +76,46 @@ export function evaluate(program: LIR.Program): LIR.Primitive {
   }
 }
 
-function constant(stack: Stack, line: LIR.Constant): Stack {
+function constant(stack: Stack, op: LIR.Constant): Stack {
   const base: number = top(stack).base_address;
-  const dest: number = base + line[LIR.Get.Dest];
-  const value: Value = { tag: "Value", value: line[LIR.Get.Left] };
+  const dest: number = base + op[LIR.Get.Dest];
+  const value: Value = { tag: "Value", value: op[LIR.Get.Left] };
   stack.data[dest] = value;
   top(stack).pc++;
   return stack;
 }
 
-function copy(stack: Stack, line: LIR.Copy): Stack {
+function copy(stack: Stack, op: LIR.Copy): Stack {
   const base: number = top(stack).base_address;
-  const dest: number = base + line[LIR.Get.Dest];
-  const source: number = base + line[LIR.Get.Left];
+  const dest: number = base + op[LIR.Get.Dest];
+  const source: number = base + op[LIR.Get.Left];
   stack.data[dest] = stack.data[source];
   top(stack).pc++;
   return stack;
 }
 
-function load(stack: Stack, line: LIR.Load): Stack {
+function load(stack: Stack, op: LIR.Load): Stack {
   const base: number = top(stack).base_address;
-  const source_ptr: Pointer = to_pointer(stack.data[line[LIR.Get.Left]]);
-  const dest: number = base + line[LIR.Get.Dest];
+  const source_ptr: Pointer = to_pointer(stack.data[op[LIR.Get.Left]]);
+  const dest: number = base + op[LIR.Get.Dest];
   stack.data[dest] = stack.data[source_ptr.address];
   top(stack).pc++;
   return stack;
 }
 
-function store(stack: Stack, line: LIR.Store): Stack {
+function store(stack: Stack, op: LIR.Store): Stack {
   const base: number = top(stack).base_address;
-  const source: number = base + line[LIR.Get.Left];
-  const dest_ptr: Pointer = to_pointer(stack.data[line[LIR.Get.Dest]]);
+  const source: number = base + op[LIR.Get.Left];
+  const dest_ptr: Pointer = to_pointer(stack.data[op[LIR.Get.Dest]]);
   stack.data[dest_ptr.address] = stack.data[source];
   top(stack).pc++;
   return stack;
 }
 
-function alloc(stack: Stack, line: LIR.Alloc): Stack {
+function alloc(stack: Stack, op: LIR.Alloc): Stack {
   const base: number = top(stack).base_address;
-  const source: number = base + line[LIR.Get.Left];
-  const dest: number = base + line[LIR.Get.Dest];
+  const source: number = base + op[LIR.Get.Left];
+  const dest: number = base + op[LIR.Get.Dest];
   const data: Data = stack.data[source];
   const ptr: number = stack.data.push(data) - 1;
   stack.data[dest] = { tag: "Pointer", address: ptr };
@@ -123,11 +123,11 @@ function alloc(stack: Stack, line: LIR.Alloc): Stack {
   return stack;
 }
 
-function add(stack: Stack, line: LIR.Add): Stack {
+function add(stack: Stack, op: LIR.Add): Stack {
   const base: number = top(stack).base_address;
-  const dest: number = base + line[LIR.Get.Dest];
-  const left: number = base + line[LIR.Get.Left];
-  const right: number = base + line[LIR.Get.Right];
+  const dest: number = base + op[LIR.Get.Dest];
+  const left: number = base + op[LIR.Get.Left];
+  const right: number = base + op[LIR.Get.Right];
   const l: Value = to_value(stack.data[left]);
   const r: Value = to_value(stack.data[right]);
   stack.data[dest] = { tag: "Value", value: l.value + r.value };
@@ -135,17 +135,17 @@ function add(stack: Stack, line: LIR.Add): Stack {
   return stack;
 }
 
-function jump(stack: Stack, line: LIR.Jump): Stack {
-  const target: number = line[LIR.Get.Left].line;
+function jump(stack: Stack, op: LIR.Jump): Stack {
+  const target: number = op[LIR.Get.Left].line;
   top(stack).pc = target;
   return stack;
 }
 
-function branch(stack: Stack, line: LIR.Branch): Stack {
+function branch(stack: Stack, op: LIR.Branch): Stack {
   const base: number = top(stack).base_address;
-  const condition: number = base + line[LIR.Get.Left];
-  const left: LIR.LineNumber = line[LIR.Get.Right][0];
-  const right: LIR.LineNumber = line[LIR.Get.Right][1];
+  const condition: number = base + op[LIR.Get.Left];
+  const left: LIR.LineNumber = op[LIR.Get.Right][0];
+  const right: LIR.LineNumber = op[LIR.Get.Right][1];
   const c: Value = to_value(stack.data[condition]);
   top(stack).pc = (c.value !== 0) ? left.line : right.line;
   return stack;
@@ -153,10 +153,10 @@ function branch(stack: Stack, line: LIR.Branch): Stack {
 
 // TODO: implement call
 
-function ret(stack: Stack, line: LIR.Return): Stack {
+function ret(stack: Stack, op: LIR.Return): Stack {
   // copy return value
   const base: number = top(stack).base_address;
-  const source: number = base + line[LIR.Get.Left];
+  const source: number = base + op[LIR.Get.Left];
   const dest: number = top(stack).return_address;
   stack.data[dest] = stack.data[source];
   stack.frames.pop();
