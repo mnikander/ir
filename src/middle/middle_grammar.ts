b2c42113f915746a83f26c228f9710b159c46950
Marco Nikander <m4n1k.developer@gmail.com>
2026-09-01 12:00:08 +0200

    ref: cleanup MIR annotations

--- src/middle/middle_grammar.ts
+++ src/middle/middle_grammar.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2026 Marco Nikander
+// Copysecond (c) 2026 Marco Nikander
 
 // A middle intermediate representation (MIR) designed for easy analysis.
 
@@ -23,31 +23,31 @@ export type Terminator   = Return | Jump | Branch;
 export type Operand      = Read | Move | Literal;
 
 export type Let          = [ tag: "let",           resource: number, value: Value ]; // define a Resource
-export type Drop         = [ tag: 'drop',          source: Move ];
-export type Return       = [ tag: 'return',        source: Operand ];
-export type Jump         = [ tag: 'jump',          target: BlockId ];
+export type Drop         = [ tag: 'drop',          Move ];
+export type Return       = [ tag: 'return',        Operand ];
+export type Jump         = [ tag: 'jump',          targetBlock: BlockId ];
 export type Branch       = [ tag: 'branch',        condition: Operand, thenBlock: BlockId, elseBlock: BlockId ];
 export type Phi          = [ tag: 'phi',           inputs: Sources];
 export type Call         = [ tag: 'call',          function: FunctionId, arguments: Arguments ];
-export type Constant     = [ tag: 'constant',      value: Literal ];
-export type Copy         = [ tag: 'copy',          value: Read | Move ];
-export type Own          = [ tag: 'own',           value: Read | Move ]; // cannot be lowered yet, LIR is missing the corresponding functionality
-export type Borrow       = [ tag: 'borrow',        value: Read | Move ]; // take the address of a resource and create a read-only pointer
-export type Dereference  = [ tag: 'dereference',   pointer: Read | Move ]; // dereference pointer and load the value
-export type Add          = [ tag: 'add',           left: Operand, right: Operand ];
-export type Subtract     = [ tag: 'subtract',      left: Operand, right: Operand ];
-export type Multiply     = [ tag: 'multiply',      left: Operand, right: Operand ];
-export type Divide       = [ tag: 'divide',        left: Operand, right: Operand ];
-export type Remainder    = [ tag: 'remainder',     left: Operand, right: Operand ];
-export type Minimum      = [ tag: 'minimum',       left: Operand, right: Operand ];
-export type Maximum      = [ tag: 'maximum',       left: Operand, right: Operand ];
-export type Negative     = [ tag: 'negate',        left: Operand ];
-export type Equal        = [ tag: 'equal',         left: Operand, right: Operand ];
-export type Unequal      = [ tag: 'unequal',       left: Operand, right: Operand ];
-export type Less         = [ tag: 'less',          left: Operand, right: Operand ];
-export type LessEqual    = [ tag: 'less_equal',    left: Operand, right: Operand ];
-export type Greater      = [ tag: 'greater',       left: Operand, right: Operand ];
-export type GreaterEqual = [ tag: 'greater_equal', left: Operand, right: Operand ];
+export type Own          = [ tag: 'own',           Read | Move ]; // cannot be lowered yet, LIR is missing the corresponding functionality
+export type Borrow       = [ tag: 'borrow',        Read | Move ]; // take the address of a resource and create a read-only pointer
+export type Dereference  = [ tag: 'dereference',   Read | Move ]; // dereference pointer and load the value
+export type Constant     = [ tag: 'constant',      Literal ];
+export type Copy         = [ tag: 'copy',          Read | Move ];
+export type Add          = [ tag: 'add',           Operand, Operand ];
+export type Subtract     = [ tag: 'subtract',      Operand, Operand ];
+export type Multiply     = [ tag: 'multiply',      Operand, Operand ];
+export type Divide       = [ tag: 'divide',        Operand, Operand ];
+export type Remainder    = [ tag: 'remainder',     Operand, Operand ];
+export type Minimum      = [ tag: 'minimum',       Operand, Operand ];
+export type Maximum      = [ tag: 'maximum',       Operand, Operand ];
+export type Negative     = [ tag: 'negate',        Operand ];
+export type Equal        = [ tag: 'equal',         Operand, Operand ];
+export type Unequal      = [ tag: 'unequal',       Operand, Operand ];
+export type Less         = [ tag: 'less',          Operand, Operand ];
+export type LessEqual    = [ tag: 'less_equal',    Operand, Operand ];
+export type Greater      = [ tag: 'greater',       Operand, Operand ];
+export type GreaterEqual = [ tag: 'greater_equal', Operand, Operand ];
 
 export type Read         = [ tag: "read",          resource: number ]; // use the value of a Resource
 export type Move         = [ tag: "move",          resource: number ]; // destructively move, i.e. consume, the Resource
@@ -55,13 +55,11 @@ export type Literal      = [ tag: "literal",       value: number ];
 export type Arguments    = [ tag: "arguments",     ...Operand[]];
 export type Sources      = [ tag: "sources",       ...From[] ];
 export type From         = [ tag: "from",          block: BlockId, resource: Operand ];
-export type FunctionId   = [ tag: "function_id",   id: number ];
-export type BlockId      = [ tag: "block_id",      id: number ];
+export type FunctionId   = [ tag: "function_id",   index: number ];
+export type BlockId      = [ tag: "block_id",      index: number ];
 
 export enum Get {
     Tag       = 0,
-    Resource  = 1,
-    Value     = 2,
-    Left      = 1, // alias to first value argument
-    Right     = 2, // alias to second value argument
+    First     = 1, // alias to first value argument
+    Second    = 2, // alias to second value argument
 }
