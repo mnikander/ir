ddffc8adc81240f61904af3c4485ff3fa99e5c52
Marco Nikander <m4n1k.developer@gmail.com>
2026-09-03 09:44:50 +0200

    ref: remove HIR implementation

--- src/analysis/validate.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026 Marco Nikander
-
-import * as HIGH from "../high/high_grammar.ts";
-import { validate_unique_variables } from "./unique_variables.ts";
-
-export function validate(program: HIGH.Program): boolean {
-  return validate_unique_variables(program);
-}
